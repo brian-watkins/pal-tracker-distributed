eb27d32b16abb5de22632c18de62c8ed4d6a30b1
Brian Watkins <bwatkins@pivotal.io>
2018-11-09 10:05:31 -0500

    Add extra bean to get rid of bean conflicts

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -1,10 +1,15 @@
 package io.pivotal.pal.tracker.registration;
 
+import io.pivotal.spring.cloud.service.config.ConfigClientOAuth2BootstrapConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import java.util.TimeZone;
@@ -25,5 +30,12 @@ public class App {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    @Primary
+    @ConditionalOnBean(ConfigClientOAuth2BootstrapConfiguration.class)
+    public OAuth2ProtectedResourceDetails details(ConfigClientOAuth2BootstrapConfiguration configClientConfig) {
+        return configClientConfig.configClientOAuth2ResourceDetails();
     }
 }
